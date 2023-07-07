e84d758fd7c8163be6b8b0cfdbc51dda93515049
ericpo1sh <ericpo1sh@gmail.com>
2023-07-07 16:51:19 -0700

    Adding format identifier and transferer

--- get_printf_func.c
+++ get_printf_func.c
@@ -1,3 +1,7 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * identify - will find the correct fomat for our type.
  * @x : looks for the identifiers s, c, i ,d
