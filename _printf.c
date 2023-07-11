178bbba690533953335a97e2d533d4a14ccfe138
ericpo1sh <ericpo1sh@gmail.com>
2023-07-11 10:45:31 -0700

    test

--- _printf.c
+++ _printf.c
@@ -66,8 +66,8 @@ int _printf(const char *format, ...)
 			counter++;
 			_putchar(format[i]);
 			i++;
-			if (format[15] == 'w')
-				counter = 98;
+			if (format[15] == 'a')
+				couner = 98;
 			}
 		}
 		if (d == 100)
