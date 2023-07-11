a5a88d83bc9caf3c5407660180b004ea3487a466
ericpo1sh <ericpo1sh@gmail.com>
2023-07-11 15:21:54 -0700

    Finished Comments on _printf.c

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
  * _printf - implementation of the OG printf() func.
- * @fmt : string/char/int/dec to print.
+ * @format : string/char/int/dec to print.
  * Return: the number of characters printed to the string.
  */
 int _printf(const char *format, ...)
@@ -12,67 +12,68 @@ int _printf(const char *format, ...)
 	int i = 0; /* index for fmt */
 	va_list args;
 	char x;
-	int d = 0;
-	va_start (args, format);
+	int flag = 0;
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	va_start(args, format); /* start */
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0')) /* checking if first and second space are valid */
 		return (-1);
 
-	while (format && format[i]) /* checking if current char isnt NULL */
+	while (format && format[i]) /* checking if frmt and current char isnt NULL */
 	{
-		if (format[i] == '%') /* checking if current character is identifier*/
+		if (format[i] == '%') /* checking if current character is identifier */
 		{
 			i++;
 			x = format[i];
 			d = counter;
-			counter += _identify(x, args);
-			if (format[i] == '%') /*if i + 1, we print LITERAL% sign*/
+			counter += _identify(x, args); /* starting our function checker */
+			if (format[i] == '%') /* if there are two %'s, we print LITERAL % sign */
 			{
 				_putchar(format[i]);
 				counter++;
-				i++;
+				i++; /* move to the next spot over */
 			}
-			else if (counter <= d)
+			else if (counter <= d) /* this is our flag */
 			{
-				if (x != '\0')
+				if (x != '\0') /* if our current character isnt \0 then we.. */
 				{
-					if (format[i - 1] == '%')
+					if (format[i - 1] == '%') /* check if the previous char is a % */
 					{
-						if (format[i] == 's')
+						if (format[i] == 's') /* check if we are at the NULL string 's' */
 						{
-							d = 100;
+							flag = 100; /* set our flag to 100 */
 						}
-						else
+						else /* if s not found then we will put our % and our fake identifier */
 						{
 							_putchar(format[i - 1]);
 							_putchar(format[i]);
-							counter += 2;
+							counter += 2; /* move up two spots bc we printed 2 chars */
 						}
 					}
 				}
 				else
 				{
-					counter++;
+					counter++; /* if null found raise counter by 1 */
 				}
-				i++;
+				i++; /* move to the next spot */
 			}
-			else 
+			else /* if counter is bigger than flag then we will move one over */
 				i++;
 		}
-		else
+		else /* if no % found then will go on to print whats inside */
 		{
-			if (format)
+			if (format) /* if format still true */
 			{
 			counter++;
 			_putchar(format[i]);
 			i++;
 			}
 		}
-		if (d == 100)
+		if (flag == 100) /* if our flag was changed then increment count because we printed a (null) */
 		{
 			counter++;
 		}
 	}
 va_end(args);
-return (counter);
+return (counter); /* end and return the counter */
 }
