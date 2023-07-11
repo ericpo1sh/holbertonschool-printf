0bee9ec308bb5d21c85b88bf23af165179be6df1
ericpo1sh <ericpo1sh@gmail.com>
2023-07-11 15:42:00 -0700

    Added comments and cleaned up _printf_functions

--- _printf_functions.c
+++ _printf_functions.c
@@ -12,7 +12,7 @@ int print_string(va_list varg)
 	int i = 0;
 	int count = 0;
 
-	if (!(string))
+	if (!(string)) /* if found null, print (null) */
 	{
 		_putchar('(');
 		_putchar('n');
@@ -20,13 +20,13 @@ int print_string(va_list varg)
 		_putchar('l');
 		_putchar('l');
 		_putchar(')');
-		return (6);
+		return (6); /* add 6 bc we printed (null) */
 	}
-	if (*string == '\0')
+	if (*string == '\0') /* if current spot is \0 return 4 */
 	{
 		return (-4);
 	}
-	if (string)
+	if (string) /* if not null, print string */
 	{
 		while (string[i])
 		{
@@ -65,32 +65,32 @@ int print_num(va_list varg)
 	{
 		if (x < 0)
 		{
-			_putchar('-');
+			_putchar('-'); /* if its a neg, add - */
 			count++;
 		}
 		numb = x;
 		len = 0;
-		while (numb != 0)
+		while (numb != 0) /* calculate the number of digits in the number */
 		{
 			numb /= 10;
 			len++;
 		}
 		powten = 1;
 		for (i = 1; i <= len - 1; i++)
-			powten *= 10;
+			powten *= 10; /* calculate the power of ten based on the number of digits */
 		for (i = 1; i <= len; i++)
 		{
-			digit = x / powten;
+			digit = x / powten; /* get the first digit */
 			if (x < 0)
-				_putchar((digit * -1) + 48);
+				_putchar((digit * -1) + 48); /* convert the digit to a character and print it */
 			else
-				_putchar(digit + '0');
+				_putchar(digit + '0'); /* convert the digit to a character and print it */
 			count++;
-			x -= digit * powten;
-			powten /= 10;
+			x -= digit * powten; /* remove the printed digit from the number */
+			powten /= 10; /* decrease the power of ten */
 		}
 	}
-	else
+	else /* if the number is 0, print 0 */
 	{
 		_putchar('0');
 		return (1);
